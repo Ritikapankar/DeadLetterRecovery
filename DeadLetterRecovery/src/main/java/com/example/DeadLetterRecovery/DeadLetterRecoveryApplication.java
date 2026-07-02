fe4c255ac8a11fc502babf5455f548d165c5834a
Ritikapankar <ritikapankar25@gmail.com>
2026-07-02 12:58:16 +0530

    initals

---
+++ DeadLetterRecovery/src/main/java/com/example/DeadLetterRecovery/DeadLetterRecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.DeadLetterRecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadLetterRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadLetterRecoveryApplication.class, args);
+	}
+
+}
