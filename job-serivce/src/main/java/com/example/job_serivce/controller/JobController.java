e9310187cac811fcc9549de5b19862f5e0a7a2a4
Ritika25@ <ritikapankar25@gmail.com>
2026-07-17 03:20:41 +0530

    only post

--- job-serivce/src/main/java/com/example/job_serivce/controller/JobController.java
+++ job-serivce/src/main/java/com/example/job_serivce/controller/JobController.java
@@ -1,5 +1,37 @@
 package com.example.job_serivce.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.job_serivce.dto.CreateJobRequest;
+import com.example.job_serivce.dto.JobResponse;
+import com.example.job_serivce.service.JobService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/jobs")
+@RequiredArgsConstructor
 public class JobController {
     
+    private final JobService jobservice;
+
+    @PostMapping
+    public ResponseEntity<JobResponse> createJob(
+        @Valid @RequestBody CreateJobRequest request) {
+        JobResponse response = jobservice.createJob(request);
+       
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+      
+    // @GetMapping
+    // public ResponseEntity<JobResponse> getAllJob(){
+
+    //     return new ResponseEntity<JobResponse>(jobservice.getAllJobs());
+    // }
 }
---
+++ job-serivce/src/main/java/com/example/job_serivce/controller/Valid.java
@@ -0,0 +1,8 @@
+package com.example.job_serivce.controller;
+
+/**
+ * Valid
+ */
+public @interface Valid {
+
+}
